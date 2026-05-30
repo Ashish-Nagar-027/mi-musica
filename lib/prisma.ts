e8642432e30db0a21836227e5e1fb36dbc367465
Ashish Nagar <ashishnagar033@gmail.com>
2026-05-30 19:04:47 +0530

    prisma setup added

---
+++ lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+// declare global{
+//     var prisma : PrismaClient
+// }
+
+const globalForPrisma = global as unknown as { prisma : PrismaClient}
+
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABESE_URL
+})
+
+
+const prisma = globalForPrisma.prisma || new PrismaClient({
+    adapter
+})
+
+if(process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
